3f5c3d6dccd3bcd941dad4bf27f4184dee1146d3
fabiotrade <fabiomaimone@gmail.com>
2025-02-04 08:12:21 -0300

    declaração de variáveis e laço de repetição for

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
+// declaração de variáveis e laço de repetição for com
+// inicialização i=0, condição i< amigos.length e incremento i++ a cada iteração
+let amigos = [];
+let lista = document.getElementById('listaAmigos');
+lista.innerHTML = '';
+for (let i = 0; i < amigos.length; i++){
+    let item = document.createElement('li')
+}
 
