4dc47857a92581662a2642ad0311712472045446
fabiotrade <fabiomaimone@gmail.com>
2025-02-04 08:25:51 -0300

    Função adicionarAmigo

--- app.js
+++ app.js
@@ -7,3 +7,22 @@ for (let i = 0; i < amigos.length; i++){
     let item = document.createElement('li')
 }
 
+// função "adicionarAmigo" e lógica de funcionamento
+function adicionarAmigo(){
+    let campoNome = document.getElementById('amigo')
+    let nome = campoNome.value;
+    if (nome === ''){
+        alert('Olá, insira um nome, por favor.')
+        return;
+    }
+        if (amigos.includes(nome)) {
+            alert('Esse nome já consta na lista, por favor, insira outro');
+            return
+        }
+    amigos.push(nome)
+    limparCampo();
+    AtualizarLista();
+    console.log(nome);
+} 
+
+
